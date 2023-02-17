5c0a11e43dd8cd8580bcab9a2d855327d9007384
Justine Enyan <enyanjustine146@gmail.com>
2023-02-17 00:06:54 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 /**
  *main-Entry point
- *Return: Always 1 (success)
+ *Return: 1 (Success)
  */
 int main(void)
 {
-fwrite(4 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
