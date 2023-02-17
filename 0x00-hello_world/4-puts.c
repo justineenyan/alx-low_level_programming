d92a0c30d2d3146b1201959293f9c3a163cd3b01
Justine Enyan <enyanjustine146@gmail.com>
2023-02-16 18:58:42 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-#include <stdio.h> int main(void) { puts("\"Programming is like building a multilingual puzzle"); return: (0); }
+#include <stdio.h>
+/**
+ * main - My first c programme
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
