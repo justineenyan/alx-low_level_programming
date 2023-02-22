31c163bbd63cdcedf66da91019d3d6602781e713
Justine Enyan <enyanjustine146@gmail.com>
2023-02-21 18:46:52 -0800

    Prints in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - prints alphabets in lowercases
+ *Return: always 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 Prints _putchar
+printing in lowercase
