f0f91904e64fb770e591859c02b95b10582fafcc
Justine Enyan <enyanjustine146@gmail.com>
2023-02-22 12:35:54 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
-		return ('-1');
 	}
+	return (-1);
 }
